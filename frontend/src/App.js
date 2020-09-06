cd95836c42bb64909e2bc42fd8f34e06f183f4c0
benas31 <b.bock@me.com>
2020-09-06 14:03:29 +0200

    delete useless stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CarList from "./views/CarList";
 import Login from "./views/Login";
-import Test from "./views/Test";
-import Dashboard from "./views/Dashboard";
 import Contract from "./views/Contract";
 import Homepage from "./views/Homepage";
 import Profil from "./views/Profil";
@@ -20,10 +18,8 @@ const App = () => {
       <Router>
         <Switch>
           <Route exact path="/" component={Homepage}></Route>
-          <Route exact path="/test" component={Test}></Route>
           <Route exact path="/carlist" component={CarList}></Route>
           <Route exact path="/login" component={Login}></Route>
-          <Route exact path="/dashboard" component={Dashboard}></Route>
           <Route exact path="/contract" component={Contract}></Route>
           <Route exact path="/profil" component={Profil}></Route>
           <Route exact path="/addcontract" component={AddContract}></Route>
