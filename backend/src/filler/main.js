9268e340b2fdd1ad43b962bf7526ab22e8fcd7a1
benas31 <b.bock@me.com>
2020-09-26 14:44:44 +0200

    Fix Filler + Filter status car

--- backend/src/filler/main.js
+++ backend/src/filler/main.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-var uristring = "mongodb://localhost/Leasing";
+var uristring = "mongodb://localhost/LeasingTest";
 
 var Car = require("../models/Car");
 var User = require("../models/User");
@@ -12,91 +12,99 @@ function fillCar() {
     chassis_number: 123,
     brand: "BMW",
     modele: "Serie 1",
-    price: 25000,
+    price: 500,
     transmission: "Manuel",
     consommation: 5.5,
     door: 4,
     fuel: "Essence",
     power_ch: 114,
     seat: 4,
-    color: "red",
+    color: "white",
+    photo : "https://storage.googleapis.com/tfeben/bmw-1.png",
     status: 0,
   });
   const car2 = Car.create({
     chassis_number: 123,
     brand: "BMW",
     modele: "Serie 2",
-    price: 30000,
+    price: 550,
     transmission: "Manuel",
     consommation: 6,
     door: 4,
     fuel: "Essence",
     power_ch: 114,
     seat: 4,
-    color: "blue",
+    color: "white",
+    photo : "https://storage.googleapis.com/tfeben/bmw-1.png",
     status: 0,
   });
   const car3 = Car.create({
     chassis_number: 123,
     brand: "Porsche",
-    modele: "Panamera afou",
-    price: 250000,
+    modele: "Panamera",
+    price: 700,
     transmission: "Manuel",
     consommation: 15.5,
     door: 4,
     fuel: "Essence",
     power_ch: 244,
     seat: 4,
-    color: "red",
+    color: "black",
+    photo: "https://storage.googleapis.com/tfeben/porsche-panamera.png",
     status: 0,
   });
   const car4 = Car.create({
     chassis_number: 123,
     brand: "Porsche",
-    modele: "Panamera afou",
-    price: 250000,
+    modele: "Panamera",
+    price: 650,
     transmission: "Manuel",
     consommation: 5.5,
     door: 4,
     fuel: "Essence",
     power_ch: 114,
     seat: 4,
-    color: "blue",
+    color: "black",
+    promo: true,
+    photo: "https://storage.googleapis.com/tfeben/porsche-panamera.png",
     status: 0,
   });
   const car5 = Car.create({
     chassis_number: 123,
     brand: "Audi",
-    modele: "Q4",
-    price: 60000,
+    modele: "RS4",
+    price: 850,
     transmission: "Auto",
     consommation: 10,
     door: 4,
     fuel: "Essence",
     power_ch: 114,
     seat: 4,
-    color: "red",
+    color: "grey",
+    photo: "https://storage.googleapis.com/tfeben/audi-rs4.png",
     status: 0,
   });
   const car6 = Car.create({
     chassis_number: 123,
     brand: "Audi",
     modele: "Q5",
-    price: 62000,
+    price: 650,
     transmission: "Auto",
     consommation: 9.5,
     door: 4,
     fuel: "Essence",
     power_ch: 114,
     seat: 4,
-    color: "red",
+    color: "black",
+    promo: true,
+    photo: "https://storage.googleapis.com/tfeben/audi-q5.png",
     status: 0,
   });
   const car7 = Car.create({
     chassis_number: 123,
     brand: "Volkswagen",
     modele: "Polo",
-    price: 23000,
+    price: 300,
     transmission: "Manuel",
     consommation: 5.5,
     door: 4,
@@ -104,13 +112,14 @@ function fillCar() {
     power_ch: 114,
     seat: 4,
     color: "red",
+    photo: "https://storage.googleapis.com/tfeben/volkswagen-polo.png",
     status: 0,
   });
   const car8 = Car.create({
     chassis_number: 123,
     brand: "Volkswagen",
-    modele: "Golf",
-    price: 27000,
+    modele: "Polo",
+    price: 300,
     transmission: "Manuel",
     consommation: 5.5,
     door: 4,
@@ -118,35 +127,114 @@ function fillCar() {
     power_ch: 114,
     seat: 4,
     color: "red",
+    photo: "https://storage.googleapis.com/tfeben/volkswagen-polo.png",
     status: 0,
   });
   const car9 = Car.create({
     chassis_number: 123,
     brand: "Volkswagen",
     modele: "T-ROC",
-    price: 30000,
+    price: 400,
     transmission: "Manuel",
     consommation: 5.5,
     door: 4,
     fuel: "Essence",
     power_ch: 114,
     seat: 4,
-    color: "red",
+    color: "green",
+    promo: true,
+    photo: "https://storage.googleapis.com/tfeben/volkswagen-troc.png",
     status: 0,
   });
   const car10 = Car.create({
     chassis_number: 123,
     brand: "Porsche",
-    modele: "Cayman",
-    price: 870654,
+    modele: "Carrera",
+    price: 1000,
     transmission: "Auto",
     consommation: 9.9,
     door: 4,
     fuel: "Essence",
     power_ch: 365,
     seat: 4,
-    color: "red",
+    color: "blue",
+    photo: "https://storage.googleapis.com/tfeben/porsche-carrera.png",
     status: 0,
+  });
+  const car11 = Car.create({
+    chassis_number: 123,
+    brand: "BMW",
+    modele: "Serie 1",
+    price: 500,
+    transmission: "Manuel",
+    consommation: 5.5,
+    door: 4,
+    fuel: "Essence",
+    power_ch: 114,
+    seat: 4,
+    color: "white",
+    photo : "https://storage.googleapis.com/tfeben/bmw-1.png",
+    status: 1,
+  });
+  const car12 = Car.create({
+    chassis_number: 123,
+    brand: "BMW",
+    modele: "Serie 1",
+    price: 500,
+    transmission: "Manuel",
+    consommation: 5.5,
+    door: 4,
+    fuel: "Essence",
+    power_ch: 114,
+    seat: 4,
+    color: "white",
+    photo : "https://storage.googleapis.com/tfeben/bmw-1.png",
+    status: 1,
+  });
+  const car13 = Car.create({
+    chassis_number: 123,
+    brand: "BMW",
+    modele: "Serie 1",
+    price: 500,
+    transmission: "Manuel",
+    consommation: 5.5,
+    door: 4,
+    fuel: "Essence",
+    power_ch: 114,
+    seat: 4,
+    color: "white",
+    photo : "https://storage.googleapis.com/tfeben/bmw-1.png",
+    status: 1,
+  });
+  const car14 = Car.create({
+    chassis_number: 123,
+    brand: "BMW",
+    modele: "Serie 1",
+    price: 500,
+    transmission: "Manuel",
+    consommation: 5.5,
+    door: 4,
+    fuel: "Essence",
+    power_ch: 114,
+    seat: 4,
+    color: "white",
+    photo : "https://storage.googleapis.com/tfeben/bmw-1.png",
+    status: 1,
+  });
+  const car15 = Car.create({
+    chassis_number: 123,
+    brand: "BMW",
+    modele: "Serie 1",
+    price: 500,
+    transmission: "Manuel",
+    consommation: 5.5,
+    door: 4,
+    fuel: "Essence",
+    power_ch: 114,
+    seat: 4,
+    color: "white",
+    photo : "https://storage.googleapis.com/tfeben/bmw-1.png",
+    status: 1,
   });
 
   return Promise.all([
@@ -160,6 +248,11 @@ function fillCar() {
     car8,
     car9,
     car10,
+    car11,
+    car12,
+    car13,
+    car14,
+    car15,
   ]);
 }
 
@@ -184,7 +277,7 @@ async function fillUser() {
     email: "vendeur@vendeur.com",
     role: "vendeur",
     lastname: "Benas",
-    firstname: "LeBock",
+    firstname: "Bock",
     phone: "027723323",
   });
   const user3 = User.create({
@@ -192,7 +285,7 @@ async function fillUser() {
     password: paswdClient,
     email: "client@client.com",
     lastname: "Xav",
-    firstname: "Leouf",
+    firstname: "Hoffmann",
     phone: "027723323",
     role: "client",
   });
@@ -201,72 +294,71 @@ async function fillUser() {
 }
 
 function fillContract() {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth();
+  const day = today.getDate();
+  const todayIn4Year = new Date(year + 4, month, day);
   User.findOne({ lastname: "Benas" }, (err, perso) => {
     User.findOne({ lastname: "Xav" }, (errc, client) => {
-      Car.findOne({ modele: "Serie 1" }, (errcar, car) => {
+      Car.find({ modele: "Serie 1", status: 1 }, (errcar, car) => {
         const contract1 = Contract.create({
-          date_start: new Date(),
-          date_end: new Date(),
+          date_start: today,
+          date_end: todayIn4Year,
           km_debut: 0,
-          km_fin: 100000,
-          prix: 23654,
+          km_fin: 0,
+          km_year: 10000,
+          prix: 500,
           actif: 1,
-          fk_car: car._id,
+          fk_car: car[0]._id,
           fk_client: client._id,
           fk_personnel: perso._id,
         });
         const contract2 = Contract.create({
-          date_start: new Date(),
-          date_end: new Date(),
+          date_start: today,
+          date_end: todayIn4Year,
           km_debut: 0,
-          km_fin: 100000,
-          prix: 23654,
+          km_fin: 0,
+          km_year: 10000,
+          prix: 500,
           actif: 1,
-          fk_car: car._id,
+          fk_car: car[1]._id,
           fk_client: client._id,
           fk_personnel: perso._id,
         });
         const contract3 = Contract.create({
-          date_start: new Date(),
-          date_end: new Date(),
+          date_start: today,
+          date_end: todayIn4Year,
           km_debut: 0,
-          km_fin: 100000,
-          prix: 23654,
+          km_fin: 0,
+          km_year: 10000,
+          prix: 500,
           actif: 1,
-          fk_car: car._id,
+          fk_car: car[2]._id,
           fk_client: client._id,
           fk_personnel: perso._id,
         });
         const contract4 = Contract.create({
-          date_start: new Date(),
-          date_end: new Date(),
+          date_start: today,
+          date_end: todayIn4Year,
           km_debut: 0,
-          km_fin: 100000,
-          prix: 23654,
+          km_fin: 0,
+          km_year: 10000,
+          prix: 500,
           actif: 1,
-          fk_car: car._id,
+          fk_car: car[3]._id,
           fk_client: client._id,
           fk_personnel: perso._id,
         });
         const contract5 = Contract.create({
-          date_start: new Date(),
-          date_end: new Date(),
+          date_start: today,
+          date_end: todayIn4Year,
           km_debut: 0,
-          km_fin: 100000,
-          prix: 23654,
+          km_fin: 0,
+          km_year: 10000,
+          prix: 500,
           actif: 1,
-          fk_car: car._id,
-          fk_client: client._id,
-          fk_personnel: perso._id,
-        });
-        const contract6 = Contract.create({
-          date_start: new Date(),
-          date_end: new Date(),
-          km_debut: 0,
-          km_fin: 100000,
-          prix: 23654,
-          actif: 1,
-          fk_car: car._id,
+          fk_car: car[4]._id,
           fk_client: client._id,
           fk_personnel: perso._id,
         });
@@ -277,7 +369,6 @@ function fillContract() {
           contract3,
           contract4,
           contract5,
-          contract6,
         ]);
       });
     });
@@ -299,10 +390,11 @@ mongoose
         console.log(new Date(), "Docs deleted");
       })
       .then(() => {
-        fillUser().then(() => {
-          fillContract();
-        });
-        fillCar();
+        fillCar().then(() => {
+          fillUser().then(() => {
+            fillContract();
+          });
+        })
       })
       .then(() => console.log(new Date(), "Docs added"));
   })
--- backend/src/routes/car.js
+++ backend/src/routes/car.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var Car = require("../models/Car");
 
 router.get("/", function (req, res) {
-  Car.find((err, cars) => {
+  Car.find({status: 0}, (err, cars) => {
     if (err) res.json({success: false, response: err});
     else res.json({success: true, response: cars});
   });
